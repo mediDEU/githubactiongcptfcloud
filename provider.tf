513f5075f693dc7300df2920e1109f97b9c779e7
mediDEU <mehdivasef@gmail.com>
2025-06-10 15:10:30 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.2.6"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.43.0"
+    }
+
+##########################################
 provider "google" {
   project = "my-project-2025-461912"
   region  = "europe-west3"
