b0ce22ed3995606407a2464f5608f86777b2bae5
mediDEU <mehdivasef@gmail.com>
2025-06-10 16:00:27 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "GOOGLE_CREDENTIALS" {
+  description = "Dummy variable for Google Cloud credentials"
+  type        = string
+  default     = ""  # Provide an empty default value since this will be overridden by the environment variable
+}
